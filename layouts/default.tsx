f86a423971d6dda0ef1d42004932ccc3c9ee57ff
Ferry Ananda Febian <ferry.a.febian@gmail.com>
2023-10-10 00:29:49 +0800

    fix nav

--- layouts/default.tsx
+++ layouts/default.tsx
@@ -9,7 +9,12 @@ export default function DefaultLayout({ children }: { children: React.ReactNode 
       <Navbar />
       <main className="container mx-auto max-w-7xl px-6 flex-grow">{children}</main>
       <footer className="w-full flex items-center justify-center py-3">
-        <Link isExternal className="flex items-center gap-1 text-current" href="https://blog.ferryops.dev/" title="ferry blog">
+        <Link
+          isExternal
+          className="flex items-center gap-1 text-current flex-col"
+          href="https://blog.ferryops.dev/"
+          title="ferry blog"
+        >
           <span className="text-default-600">ferryops.dev 🌟</span>
           <p className="text-primary">Dibuat dengan Next.js dan TailwindCSS</p>
         </Link>
